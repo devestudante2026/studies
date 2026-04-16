3c31bb24e4e05e5cbf02b669fed2f95315b818c4
dev_estudante <dev_estudante_2026@gmail.com>
2026-04-16 18:40:26 -0300

    feat:resolve exercício de converter texto para maiúsculas com decorator

---
+++ studies/decorators/02_converte_maiusculas.py
@@ -0,0 +1,11 @@
+def upper_converter(func):
+    def wrapper(*args, **kwargs):
+        formatted = func(*args, **kwargs).upper()
+        return formatted
+    return wrapper
+
+@upper_converter
+def my_msg(msg):
+    return msg
+
+my_msg("it's all small here...")
